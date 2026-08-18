123d3d857bb0cb18f2971fbde2a1813a486f915e
aiversen2007-a11y <aiversen2007@gmail.com>
2026-08-18 12:28:06 +0000

    Add internet connection check

--- linux-info/info.sh
+++ linux-info/info.sh
@@ -10,4 +10,11 @@ echo "Diskplads:"
 df -h /
 echo "RAM:"
 free -h
+echo "Internet:"
+if ping -c 1 google.com > /dev/null 2>&1
+then
+echo "Internet virker!"
+else 
+echo "ingen internetforbindelse!"
+fi
 
